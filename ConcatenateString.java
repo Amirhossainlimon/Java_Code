8463544f312c13e9a5ec2fcecdf6c667eccecd69
Amir Hossain Limon <146566739+Amirhossainlimon@users.noreply.github.com>
2025-11-10 21:23:15 +0600

    Add files via upload

---
+++ ConcatenateString.java
@@ -0,0 +1,8 @@
+public class ConcatenateString {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "World";
+        String result = s1.concat(" " + s2);
+        System.out.println("After concatenation: " + result);
+    }
+}
